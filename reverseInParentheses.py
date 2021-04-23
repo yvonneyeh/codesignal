fc0eb3c0255df4d31bd289921eb27185e6a2ac92
Yvonne Yeh <code@yvonneyeh.com>
2021-04-22 20:19:54 -0700

    Reverse characters in nested ()

--- reverseInParentheses.py
+++ reverseInParentheses.py
@@ -1,9 +1,30 @@
+# Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+# Input strings will always be well-formed with matching ()s.
+
 def reverseInParentheses(inputString):
 
     # look at all characters
     # add everything within () into a stack
     # pop items from stack into new string
-    pass
+    stack = []
+    for x in inputString:
+        if x ==")":
+            temp =""
+            while stack[-1] !="(":
+                temp += stack.pop()
+            stack.pop() # pop the (
+            for item in temp:
+                stack.append(item)
+        else:
+            stack.append(x)
+
+    return"".join(stack)
+
+
+def reverseInParentheses_1Line(inputString):
+    return eval('"' + s.replace('(', '"+("').replace(')', '")[::-1]+"') + '"')
+
 
 def reverseInParentheses_Recursive(inputString):
     for i in range(len(inputString)):
