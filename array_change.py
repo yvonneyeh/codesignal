2f608b54ea075ce060aa5468fc923d240acb1f9e
Yvonne Yeh <code@yvonneyeh.com>
2021-04-27 14:57:05 -0700

    Array Change problem

--- array_change.py
+++ array_change.py
@@ -1,3 +1,10 @@
 # You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
 
 def arrayChange(inputArray):
+    a=0
+    newArray = inputArray[:]
+    for i in range(len(inputArray)-1):
+        if newArray[i] >= newArray[i+1]:
+            a += newArray[i] - newArray[i+1]+1
+            newArray[i+1] = newArray[i]+1
+    return a
