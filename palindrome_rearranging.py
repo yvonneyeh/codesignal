bde1d316ed32a178f2d3d921e0ae2f0885926a2f
Yvonne Yeh <code@yvonneyeh.com>
2021-04-27 15:00:49 -0700

    Pass 6/10 tests

--- palindrome_rearranging.py
+++ palindrome_rearranging.py
@@ -1,3 +1,7 @@
 # Given a string, find out if its characters can be rearranged to form a palindrome.
 
 def palindromeRearranging(inputString):
+
+    return (len(inputString) / 2 ) == len(set(inputString))
+
+    # passes 6/10 tests
