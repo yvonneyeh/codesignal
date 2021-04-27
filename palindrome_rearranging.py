42592d8f5eb77bdc26e53b58b31f775a75908c5c
Yvonne Yeh <code@yvonneyeh.com>
2021-04-27 15:05:16 -0700

    Rearrange chracters into palindrome

--- palindrome_rearranging.py
+++ palindrome_rearranging.py
@@ -10,7 +10,7 @@ def palindromeRearranging1(inputString):
 
     # passes 10/10 tests
 
-def palindromeRearranging(inputString):
+def palindromeRearranging2(inputString):
 
     counted_str=[]
     done=[]
@@ -21,6 +21,22 @@ def palindromeRearranging(inputString):
             counted_str.append(inputString.count(i))
             done.append(i)
     if len(counted_str)>1:
+        return False
+    else:
+        return True
+
+def palindromeRearranging(inputString):
+
+    char_occur = {}
+
+    for c in inputString:
+        if c not in char_occur:
+            char_occur[c] = 1
+        else:
+            char_occur[c] += 1
+
+    odd_occ = [l for l in char_occur if char_occur[l] % 2]
+    if len(odd_occ) > 1:
         return False
     else:
         return True
