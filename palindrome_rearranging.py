07ff0b542c3740d24706b658b1b9d5114fcadac3
Yvonne Yeh <code@yvonneyeh.com>
2021-04-27 15:02:26 -0700

    Pass 10/10 tests

--- palindrome_rearranging.py
+++ palindrome_rearranging.py
@@ -2,6 +2,10 @@
 
 def palindromeRearranging(inputString):
 
-    return (len(inputString) / 2 ) == len(set(inputString))
+    # return (len(inputString) / 2 ) == len(set(inputString))
 
     # passes 6/10 tests
+
+    return sum([inputString.count(i)%2 for i in set(inputString)]) <= 1
+
+    # passes 10/10 tests
