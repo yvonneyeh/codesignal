392055f1e00d7d4188dc8f6b1ca3431ba94a1e81
Yvonne Yeh <code@yvonneyeh.com>
2021-04-27 15:03:39 -0700

    Rearrange chracters into palindrome

--- palindrome_rearranging.py
+++ palindrome_rearranging.py
@@ -1,6 +1,6 @@
 # Given a string, find out if its characters can be rearranged to form a palindrome.
 
-def palindromeRearranging(inputString):
+def palindromeRearranging1(inputString):
 
     # return (len(inputString) / 2 ) == len(set(inputString))
 
@@ -9,3 +9,18 @@ def palindromeRearranging(inputString):
     return sum([inputString.count(i)%2 for i in set(inputString)]) <= 1
 
     # passes 10/10 tests
+
+def palindromeRearranging(inputString):
+
+    counted_str=[]
+    done=[]
+    for i in inputString:
+        if i in done:
+            continue
+        if inputString.count(i)%2==1:
+            counted_str.append(inputString.count(i))
+            done.append(i)
+    if len(counted_str)>1:
+        return False
+    else:
+        return True
