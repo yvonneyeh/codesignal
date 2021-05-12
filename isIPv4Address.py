7faff5db29180713fee131d9f67bd15ce166532a
Yvonne Yeh <code@yvonneyeh.com>
2021-05-11 18:52:44 -0700

    RegEx

--- isIPv4Address.py
+++ isIPv4Address.py
@@ -1,4 +1,7 @@
-def isIPv4Address(inputString):
+# An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+# Given a string, find out if it satisfies the IPv4 address naming rules.
+
+def isIPv4Address1(inputString):
 
     numbers = inputString.split('.')
     print(numbers)
@@ -17,3 +20,20 @@ def isIPv4Address(inputString):
     except ValueError:
         return False
     return True
+
+# Pass 18/21 tests
+
+def isIPv4Address(inputString):
+
+    # Make a regular expression
+    # for validating an Ip-address
+    regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
+
+    # pass the regular expression
+    # and the string in search() method
+    if(re.search(regex, inputString)):
+        return True
+
+    else:
+        return False
+    
